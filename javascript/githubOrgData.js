e4cdc9c3b2c5f9e5c0e4afa1c5e77d4f57b14907
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-01 15:51:39 -0400

    Created a .on('click', function () to get the value of my input and give me access to the files I need to finish the project.

--- javascript/githubOrgData.js
+++ javascript/githubOrgData.js
@@ -2,16 +2,28 @@
     'use strict';
     window.github = window.github || {};
     window.github.data = [];
+    $('.load')
+        .on('click', function tokenPush() {
+            var myToken = $('#token').val()
+            console.log(myToken)
+            $.ajax({
+                    url: 'https://api.github.com/users/lengstrom/orgs',
+                    method: 'GET',
+                    dataType: 'json',
+                    headers: {
+                        Authorization: 'token ' + myToken //never Commit a token
+                    }
 
-    $.ajax({
-            url: 'https://api.github.com/users/lengstrom/orgs',
-            method: 'GET',
-            dataType: 'json'
+                })
+                .done(function handleLoad(data) {
+                    console.log('data passed', data)
+                    window.github.data.push(data);
+                })
+                .fail(function dataFail(xhr) {
+                    console.log('data failed', xhr);
+                })
         })
-        .done(function handleLoad(data) {
-            console.log('data passed', data)
-            window.github.data.push(data);
-        });
+
 
 
 }());
--- javascript/githubOrgUI.js
+++ javascript/githubOrgUI.js
@@ -1,6 +1,7 @@
 (function() {
     'use strict';
     window.github = window.github || {};
+    
     console.log(window.github.data);
 
 }());
